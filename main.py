3007bd821dd64c8fc71346794af189b582832819
dev gitlap <dev@ethermon.app>
2022-09-12 15:55:17 +0700

    check volum lay tu san binance, them chuc nang hien thi danh sach token co gia hien tai giam tren 80% so voi ATH

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import os
 import json
 
-list_cap = 300
+list_cap = 500
 day_cutoff = 90
 # Minutely data will be used for duration within 1 day,
 # Hourly data will be used for duration between 1 day to 90 days,
@@ -15,22 +15,26 @@ day_cutoff = 90
 against_currency = "usd"
 coin_list = {}
 outlier_list = {}
-dataDict = {} #main dict for JSON export
+dataDict = {}  # main dict for JSON export
 
 
 end_point = 'https://api.coingecko.com/api/v3/coins/'
-volume_end_point = end_point + '{}/market_chart?vs_currency={}&days={}' # .format inside later for loop since i is the coin_id
-coin_list_end_point = end_point + ('/markets?vs_currency=usd&order=market_cap_desc&per_page={}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d').format(list_cap)
+# .format inside later for loop since i is the coin_id
+volume_end_point = end_point + '{}/market_chart?vs_currency={}&days={}'
+coin_list_end_point = end_point + \
+	('/markets?vs_currency=usd&order=market_cap_desc&per_page={}&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d').format(list_cap)
+
 
 def get_data(url):
 	api_response = requests.get(url)
 
 	try:
 		api_response.raise_for_status()
-	except requests.exceptions.HTTPError as e: # Whoops it wasn't a 200
+	except requests.exceptions.HTTPError as e:  # Whoops it wasn't a 200
 		return "Error: " + str(e)
 
 	return api_response.json()
+
 
 def export_JSON(directory, dict_name):
 	os.remove(directory)
@@ -39,21 +43,60 @@ def export_JSON(directory, dict_name):
 	    # sort key = False to remain the key order
 	    json.dump(dict_name, f, indent=4, sort_keys=False)
 
-def get_coin_list(): #get a list of top 100 coin with their id and symbol.
+
+def get_binance_live_volume(coin_ids):
+	# api_data = get_data((live_volume).format(coin_ids))
+	time.sleep(3)
+	binance_api = 'https://api.coingecko.com/api/v3/exchanges/binance/tickers?coin_ids='+coin_ids
+	api_data = get_data(binance_api)
+	vol_data = 0.0
+	vol_arr = []
+
+	# print('check coin:', coin_ids)
+	try:
+
+		for i in api_data['tickers']:
+			# print(coin_ids, i['converted_volume'])
+			vol_arr.append(i['converted_volume']['usd'])
+
+		vol_data = np.sum(vol_arr)
+
+		# print('vol_arr', vol_arr, ' => ', vol_data)
+	except Exception as e:
+		print(e)
+		print(api_data['tickers'])
+		print("ok error get_binance_live_volume")
+
+	# print("")
+	# print("get_binance_live_volume ==> ")
+	# print(binance_api)
+	# print(api_data)
+	# print("==> END <== ")
+	# print("")
+	return vol_data
+
+
+def get_coin_list():  # get a list of top 100 coin with their id and symbol.
 
 	api_data = get_data(coin_list_end_point)
 	api_response = requests.get(coin_list_end_point)
 	api_data = api_response.json()
 
-
 	for i in api_data:
-		coin_list[i['id']] = {}  #i[id] is the coin id
+		if i['symbol'] == 'usdt' or i['symbol'] == 'busd' or i['symbol'] == 'dai' or i['symbol'] == 'usdc' or i['symbol'] == 'usdp' or i['symbol'] == 'cusdc' or i['symbol'] == 'usdd' or i['symbol'] == 'cdai' or i['symbol'] == 'usdn' or i['symbol'] == 'tusd':
+			continue
+
+		coin_list[i['id']] = {}  # i[id] is the coin id
 		coin_list[i['id']]['symbol'] = (i['symbol']).upper()
 
 		# Save the price data
 		coin_list[i['id']]['hour'] = i['price_change_percentage_1h_in_currency']
 		coin_list[i['id']]['day'] = i['price_change_percentage_24h_in_currency']
 		coin_list[i['id']]['week'] = i['price_change_percentage_7d_in_currency']
+		coin_list[i['id']]['ath_change_percentage'] = i['ath_change_percentage'] # % thay doi gia hien tai so voi gia cao nhat duoc thiet lap
+
+		volume_at_binance = get_binance_live_volume(i['id'])
+		coin_list[i['id']]['volume_at_binance'] = float(volume_at_binance)
 
 		time_list = ['hour', 'day', 'week']
 		for n in time_list:
@@ -62,20 +105,24 @@ def get_coin_list(): #get a list of top 100 coin with their id and symbol.
 			else:
 				coin_list[i['id']][n] = 'NA'
 
+		coin_list[i['id']]['fulldata'] = i
+
 	# print (coin_list)
 	return coin_list
+
 
 def get_std(i):
 
 	# i is the coin_id
-	api_data = get_data((volume_end_point).format(i, against_currency, day_cutoff))
+	api_data = get_data((volume_end_point).format(
+		i, against_currency, day_cutoff))
 
 	# get raw volumes into the list volume_data
 	volume_data = []
 
 	try:
 		for n in api_data['total_volumes']:
-			volume_data.append(n[1]) #n[0] is the time code
+			volume_data.append(n[1])  # n[0] is the time code
 
 		# the last one is the latest hourly volume,
 		coin_list[i]['volume_std'] = np.std(volume_data[:-1])
@@ -88,8 +135,16 @@ def get_std(i):
 			coin_list[i]['upper_std'] = upper_std
 		else:
 			coin_list[i]['upper_std'] = 0
+
+		upper_std_2 = 0
+		upper_std_2 = round(((coin_list[i]['volume_at_binance'] - coin_list[i]['volume_mean']) / coin_list[i]['volume_std']), 2)
+
+		coin_list[i]['upper_std_2'] = upper_std_2
+
 	except Exception as e:
-		print (e)
+		print(e)
+		print(i)
+		print("ok error get_std")
 
 	# coin_list[i]['upper_std'] = round(((coin_list[i]['last_24hour_volume'] - coin_list[i]['volume_mean']) / coin_list[i]['volume_std']), 2)
 
@@ -101,7 +156,7 @@ for i in coin_list:
 	get_std(i)
 
 	dataDict[i] = coin_list[i]
-	print (i, coin_list[i])
+	print(i, coin_list[i])
 
 	#check is std or mean is NaN,
 	# if np.isnan(coin_list[i]['volume_std']) or np.isnan(coin_list[i]['volume_mean']) == False:
@@ -113,7 +168,7 @@ for i in coin_list:
 	# 	pass
 
 	# api limit @ 100 fetch per minute
-	time.sleep(1.0)
+	time.sleep(3.0)
 
 # save time to dict
 now = datetime.now()
--- static/style/js/index.js
+++ static/style/js/index.js
@@ -1,61 +1,102 @@
 async function getData(std_cap) {
-    const results = await fetch('volumeData.json');
-    const dataObj = await results.json();
+  const results = await fetch('volumeData.json');
+  const dataObj = _.sortBy(await results.json(), 'ath_change_percentage');
 
-    // get keys and push into a new array, remove the time one, and filter out the ones below std_cap
-    let objKeys =
-        Object.keys(dataObj)
-        .filter(key => key !== 'time' && dataObj[key].upper_std >= std_cap);
+  // get keys and push into a new array, remove the time one, and filter out the ones below std_cap
 
+  // let objKeys = Object.keys(dataObj).filter(key => key !== 'time' && dataObj[key].upper_std >= std_cap); // old code
 
-    //    Data Set Format for dataObj['bitcoin']
-    //    last_hour_volume: 16632680789.936144
-    //    upper_std: -0.9745
-    //    volume_mean: 25002672636.009663
-    //    volume_std: 8589051940.060508
-    //    1h: 0.23
+  let objKeys = Object.keys(dataObj).filter((key) => key !== 'time' && dataObj[key].upper_std_2 >= std_cap);
 
+  let ahtObjKeys = Object.keys(dataObj).filter((key) => key !== 'time' && dataObj[key].ath_change_percentage <= -80);
 
-    // .map the keys to get html content and generate html content
+	// let sortDataObj = _.sortBy(dataObj, 'ath_change_percentage');
 
-    const cards = objKeys.map(key => {
-        return `<div class="card">
-                    <a target="_blank" data-coingeco="https://coingecko.com/en/coins/${key}" href="https://www.tradingview.com/chart/?symbol=${dataObj[key].symbol}USDT" class="custom-card"><div class="card-body">
-                        <h2 class="card-title">${dataObj[key].symbol}</h2>
-                        <hr>
-                        <h5>${dataObj[key].upper_std}</h5>
-                        <p>
-                        <span ${dataObj[key].hour >= 0 ? `class="green"` : `class="red"`}>1H : ${dataObj[key].hour >= 0 ? `+` : ``}${dataObj[key].hour} % </span><br>
-                        <span ${dataObj[key].day >= 0 ? `class="green"` : `class="red"`}>1D : ${dataObj[key].day >= 0 ? `+` : ``}${dataObj[key].day} % </span><br>
-                        <span ${dataObj[key].week >= 0 ? `class="green"` : `class="red"`}>1W : ${dataObj[key].week >= 0 ? `+` : ``}${dataObj[key].week} % </span>
-                        </p>
-                    </div>
-                </div></a>`
-    }).join('');
-    const cardRow = document.querySelector('.card-row');
-    const timeRow = document.querySelector('.donate-row .time');
-    timeRow.textContent = `Last update: ${dataObj['time']}`
-    cardRow.innerHTML = cards;
+  // console.log('newAHT', newAHT);
+
+  //    Data Set Format for dataObj['bitcoin']
+  //    last_hour_volume: 16632680789.936144
+  //    upper_std: -0.9745
+  //    volume_mean: 25002672636.009663
+  //    volume_std: 8589051940.060508
+  //    1h: 0.23
+
+  // .map the keys to get html content and generate html content
+
+  const cards = objKeys
+    .map((key) => {
+      return `<div class="card">
+                    <a target="_blank" data-coingeco="https://coingecko.com/en/coins/${key}" href="https://www.tradingview.com/chart/?symbol=${
+        dataObj[key].symbol
+      }USDT" class="custom-card">
+                        <div class="card-body">
+                            <h2 class="card-title">${dataObj[key].symbol}</h2>
+                            <hr>
+                            <h5>${dataObj[key].upper_std}</h5>
+                            <p>
+                            <span ${dataObj[key].hour >= 0 ? `class="green"` : `class="red"`}>1H : ${
+        dataObj[key].hour >= 0 ? `+` : ``
+      }${dataObj[key].hour} % </span><br>
+                                            <span ${dataObj[key].day >= 0 ? `class="green"` : `class="red"`}>1D : ${
+        dataObj[key].day >= 0 ? `+` : ``
+      }${dataObj[key].day} % </span><br>
+                                            <span ${dataObj[key].week >= 0 ? `class="green"` : `class="red"`}>1W : ${
+        dataObj[key].week >= 0 ? `+` : ``
+      }${dataObj[key].week} % </span>
+                            </p>
+                        </div>
+                    </a>
+                </div>`;
+    })
+    .join('');
+  const cardRow = document.querySelector('.card-row');
+  cardRow.innerHTML = cards;
+
+  const cardsATH = ahtObjKeys
+    .map((key) => {
+      return `<div class="card">
+                    <a target="_blank" data-coingeco="https://coingecko.com/en/coins/${key}" href="https://www.tradingview.com/chart/?symbol=${
+        dataObj[key].symbol
+      }USDT" class="custom-card">
+                        <div class="card-body">
+                            <h2 class="card-title">${dataObj[key].symbol}</h2>
+                            <hr>
+                            <h6>price: $${dataObj[key].fulldata.current_price}</h6>
+                            <h5 class="${dataObj[key].ath_change_percentage < 0 ? 'red' : 'blue'}">${
+        dataObj[key].ath_change_percentage
+      } %</h5>
+                        </div>
+                    </a>
+                </div>`;
+    })
+    .join('');
+  const cardATHRow = document.querySelector('.ath-percent');
+  cardATHRow.innerHTML = cardsATH;
+
+  const timeRow = document.querySelector('.donate-row .time');
+  timeRow.textContent = `Last update: ${dataObj['time']}`;
 }
 
-let slider = document.querySelector('.custom-range')
+let slider = document.querySelector('.custom-range');
 
-//divide by 100 since the html range is 0 to 50 
-stdMulti.innerHTML = (slider.valueAsNumber) / 10
+//divide by 100 since the html range is 0 to 50
+stdMulti.innerHTML = slider.valueAsNumber / 10;
 
 function refresh() {
-    rangeValue = (slider.valueAsNumber) / 10
+  rangeValue = slider.valueAsNumber / 10;
 
-    // divided by 10 to since the HTML slider range is 0 to 50
-    stdMulti.innerHTML = rangeValue
+  // divided by 10 to since the HTML slider range is 0 to 50
+  stdMulti.innerHTML = rangeValue;
 
-    //re-run getData() with the input value param
-    getData(rangeValue)
+  //re-run getData() with the input value param
+  getData(rangeValue);
 }
 
 //refresh when it moves
-slider.addEventListener('input', refresh)
+slider.addEventListener('input', refresh);
 
-getData((slider.valueAsNumber) / 10);
+getData(slider.valueAsNumber / 10);
 
-let loop = setInterval(function () { refresh(); }, 30 * 60 * 1000); // 30min
+let loop = setInterval(function () {
+  refresh();
+}, 30 * 60 * 1000); // 30min
